d9da5f3480c83b54e560af3d712bd43b46a982d9
DeniseCavalcante <32266710+DeniseCavalcante@users.noreply.github.com>
2018-10-22 21:29:28 -0300

    javascript metodo

---
+++ Projeto/scripts.js
@@ -0,0 +1,27 @@
+//<a class="link" href="/conta"></a>
+
+/*const linkCriarUmaConta = document.createElement("a")
+linkCriarUmaConta.className="link"
+linkCriarUmaConta.href="/conta"
+linkCriarUmaConta.appendChild(document.createTextNode("criar uma conta"))
+
+//<a class="link" href="/longin">fazer login</a>
+
+const linkFazerLogin= document.createElement("a")
+linkFazerLogin.className="link"
+linkFazerLogin.href="/login"
+linkFazerLogin.appendChild(document.createTextNode("fazer login"))
+
+
+const formularioLogin = document.createElement("form")
+formularioLogin.appendChild(linkCriarUmaConta)
+formularioLogin.appendChild(linkFazerLogin)
+
+const divisaoProjeto = document.getElementById("projeto")
+
+divisaoProjeto.appendChild(linkCriarUmaConta)
+divisaoProjeto.appendChild(linkFazerLogin)
+divisaoProjeto.appendChild(formularioLogin)
+console.log(divisaoProjeto)*/
+
+
